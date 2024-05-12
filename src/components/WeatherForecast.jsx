f8c740d8c52134d5ebbbead3430d2b267863ee33
brian <briantaveras99@gmail.com>
2024-05-12 18:59:43 -0400

    Initial commit

---
+++ src/components/WeatherForecast.jsx
@@ -0,0 +1,14 @@
+const WeatherData = ({ day, img, imgAlt, conditions, time }) => {
+    return (
+      <div className="weather">
+        <h2>{day}</h2>
+        <img src={img} alt={imgAlt} />
+        <p><span>Conditions: </span>{conditions}</p>
+        <p><span>Time: </span>{time}</p>
+      </div>
+    );
+  };
+  
+  
+  
+export default WeatherData;
